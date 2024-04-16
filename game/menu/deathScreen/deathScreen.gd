93b441dc6c78e952bdea8058f31ce43e836560a8
Lon60 <gitlon@proton.me>
2024-04-16 12:10:42 +0200

    gameloop finally closed (lobby->lvl->lvl1->death->lobby)

--- game/menu/deathScreen/deathScreen.gd
+++ game/menu/deathScreen/deathScreen.gd
@@ -3,8 +3,5 @@ extends Node2D
 @onready var back_btn = $MarginContainer/HBoxContainer/VBoxContainer/backBtn as Button
 @onready var mainMenu = preload("res://menu/mainMenu.tscn")
 
-func _ready():
-	back_btn.pressed.connect(self._back_btn_pressed)
-	
-func _back_btn_pressed():
+func _on_back_btn_pressed():
 	get_tree().change_scene_to_packed(mainMenu)
--- game/menu/mainMenu.gd
+++ game/menu/mainMenu.gd
@@ -1,11 +1,8 @@
 extends Node2D
 
 @onready var play_btn = $MarginContainer/HBoxContainer/HBoxContainer/VBoxContainer/PlayBtn as Button
-@onready var levelMenu = preload("res://menu/levelMenu/levelMenu.tscn")
+@onready var levelMenu = load("res://menu/levelMenu/levelMenu.tscn").instantiate()
 
 
-func _ready():
-	play_btn.pressed.connect(self._play_btn_pressed)
-	
-func _play_btn_pressed():
-	get_tree().change_scene_to_packed(levelMenu)
+func _on_play_btn_pressed():
+	get_tree().get_root().add_child(levelMenu)
