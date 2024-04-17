555186e0bd68466ddeba8daadf2e58a8204dda7c
Lon60 <gitlon@proton.me>
2024-04-17 15:00:48 +0200

    add cave trap

--- game/items/caveStrap/caveStrap.gd
+++ game/items/caveStrap/caveStrap.gd
@@ -1,15 +1,29 @@
-extends Area2D
+extends RigidBody2D
 
-@onready var cave_trap: Area2D = $"."
+@onready var cave_trap: RigidBody2D = $"."
+@onready var cpu_particles_2d: CPUParticles2D = $CPUParticles2D
+@onready var png = $png
+
+
 var playerDetected: bool = false
 
 func _process(delta):
 	if playerDetected:
-		cave_trap.position.y += 100 * delta
+		cave_trap.freeze = false
 
 func _on_dedect_body_entered(body):
 	if body && body.is_in_group("player"):
 		playerDetected = true
+		
+func _on_cave_trap__body_entered(body):
+	if !(body == cave_trap):
+		call_deferred("despawn")
+		if body && body.is_in_group("player"):
+			body.dead = true
 
-func _on_area_entered(area):
-	print("dedect!")
+func despawn():
+	cpu_particles_2d.emitting = true
+	await get_tree().create_timer(0.2).timeout
+	png.visible = false
+	await get_tree().create_timer(0.55).timeout
+	queue_free()
