bbc901a46babcbb75213bf5bddf4bf43e12a5419
Lon60 <gitlon@proton.me>
2024-04-17 15:59:44 +0200

    checkpoint

--- game/levels/level2/level2.gd
+++ game/levels/level2/level2.gd
@@ -1,7 +1,14 @@
 extends Node2D
 
 @onready var player: CharacterBody2D = $player
+@onready var cave_spawn_point = $"Cave SpawnPoint"
 
 func _process(delta):
 	if player.position.y > 1000:
 		player.dead = true
+
+
+func _on_cave_entry_body_entered(body):
+	if body && body.is_in_group("player"):
+		body.position = cave_spawn_point.position
+
