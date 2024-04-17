e14c8e4297ce3a44677dca955e66adb6801f4850
Lon60 <gitlon@proton.me>
2024-04-17 08:27:05 +0200

    3 lvl system

--- game/menu/levelMenu/levelMenu.gd
+++ game/menu/levelMenu/levelMenu.gd
@@ -1,16 +1,18 @@
 extends Node2D
 
 var lvl1 = "res://levels/level1/level1.tscn"
+var lvl2 = "res://levels/level2/level2.tscn"
+var lvl3 = "res://levels/level3/level3.tscn"
 var mainMenu = "res://menu/mainMenu.tscn"
 
 func _on_lvl_1_btn_pressed():
 	SceneSwitcher.switch_scene(lvl1)
 	
 func _on_lvl_2_btn_pressed():
-	pass
+	SceneSwitcher.switch_scene(lvl2)
 
 func _on_lvl_3_btn_pressed():
-	pass
+	SceneSwitcher.switch_scene(lvl3)
 
 func _on_back_btn_pressed():
 	SceneSwitcher.switch_scene(mainMenu)
