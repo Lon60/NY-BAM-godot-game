f531127a59d68b86e54ad8bf4efadbc937e8dcb6
Lon60 <gitlon@proton.me>
2024-04-18 10:34:38 +0200

    add quit button func

--- game/menu/mainMenu.gd
+++ game/menu/mainMenu.gd
@@ -6,3 +6,7 @@ var levelMenu: String = "res://menu/levelMenu/levelMenu.tscn"
 func _on_play_btn_pressed():
 	SceneSwitcher.switch_scene(levelMenu)
 
+
+
+func _on_button_3_pressed():
+	get_tree().quit()
