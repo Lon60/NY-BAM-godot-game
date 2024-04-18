92d69964a2303e33a99ecd2d6252237791e39d63
Lon60 <gitlon@proton.me>
2024-04-18 15:45:50 +0200

    checkpoint

--- game/items/bouncePad/bouncePad.gd
+++ game/items/bouncePad/bouncePad.gd
@@ -1,10 +1,8 @@
 extends Node2D
 
+@export var yBounce = -175
 
 
 func _on_area_2d_body_entered(body):
 	if body && body.is_in_group("player"):
-		print(body.velocity.y)
-		body.velocity.y -= 175
-		print(body.velocity.y)
-		print("---")
+		body.velocity.y += yBounce
