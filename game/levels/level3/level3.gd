378cb04f6a6d402e376a1099a7cf429b7d08181b
Lon60 <gitlon@proton.me>
2024-04-18 16:13:40 +0200

    checkpoint

--- game/levels/level3/level3.gd
+++ game/levels/level3/level3.gd
@@ -5,3 +5,8 @@ extends Node2D
 func _process(delta):
 	if player.position.y > 1000:
 		player.dead = true
+
+
+func _on_player_dedect_body_entered(body):
+	if body && body.is_in_group("player"):
+		body.dead = true
