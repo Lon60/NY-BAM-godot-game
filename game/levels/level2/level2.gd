d53d4bfb15181236e96f582241aab1670a6a1184
Lon60 <gitlon@proton.me>
2024-04-18 08:58:21 +0200

    add caveExit

--- game/levels/level2/level2.gd
+++ game/levels/level2/level2.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var player: CharacterBody2D = $player
 @onready var cave_spawn_point = $"Cave SpawnPoint"
+@onready var cave_exit_spawn_point = $"CaveExit SpawnPoint"
 
 func _process(delta):
 	if player.position.y > 1000:
@@ -16,3 +17,12 @@ func _on_cave_entry_body_entered(body):
 		await get_tree().create_timer(0.5).timeout
 		body.playIrisOut()
 
+
+
+func _on_cave_exit_body_entered(body):
+	if body && body.is_in_group("player"):
+		body.playIrisIn()
+		await get_tree().create_timer(1).timeout
+		body.position = cave_exit_spawn_point.position
+		await get_tree().create_timer(0.5).timeout
+		body.playIrisOut()
