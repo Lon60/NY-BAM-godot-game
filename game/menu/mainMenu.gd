4559bf606ba76c18d736f374b62f70ee38e53f06
Lon60 <gitlon@proton.me>
2024-04-18 11:45:12 +0200

    checkpoint

--- game/menu/mainMenu.gd
+++ game/menu/mainMenu.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 var levelMenu: String = "res://menu/levelMenu/levelMenu.tscn"
-
+var settings = "res://menu/settings/settings.tscn"
 
 func _on_play_btn_pressed():
 	SceneSwitcher.switch_scene(levelMenu)
@@ -10,3 +10,7 @@ func _on_play_btn_pressed():
 
 func _on_button_3_pressed():
 	get_tree().quit()
+
+
+func _on_settings_btn_pressed():
+	SceneSwitcher.switch_scene(settings)
