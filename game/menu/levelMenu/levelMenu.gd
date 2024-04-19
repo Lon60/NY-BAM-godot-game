4917b913985037e35b3954e36358826a12643b5f
Lon60 <gitlon@proton.me>
2024-04-19 14:10:24 +0200

    checkpoint

--- game/menu/levelMenu/levelMenu.gd
+++ game/menu/levelMenu/levelMenu.gd
@@ -14,7 +14,9 @@ var mainMenu: String = "res://menu/mainMenu.tscn"
 @onready var level_2: Control = $MarginContainer/HBoxContainer/Level2
 @onready var level_3: Control = $MarginContainer/HBoxContainer/Level3
 const lvl2_png = preload("res://menu/levelMenu/level2.png")
+const lvl3_png = preload("res://menu/levelMenu/level3.png")
 @onready var lvl2_padlock = $MarginContainer/HBoxContainer/Level2/Level2/HBoxContainer/VBoxContainer/padlock
+@onready var lvl3_padlock = $MarginContainer/HBoxContainer/Level3/Level3/HBoxContainer/VBoxContainer/padlock
 
 func _process(delta):
 	if GlobalController.canAccessLvl(2):
@@ -24,8 +26,11 @@ func _process(delta):
 		level_2.add_theme_stylebox_override("panel", new_stylebox)
 		lvl2_padlock.hide()
 	if GlobalController.canAccessLvl(3):
+		var new_stylebox = StyleBoxTexture.new()
+		new_stylebox.texture = lvl3_png
 		lvl_3_btn.disabled = false
-		
+		level_3.add_theme_stylebox_override("panel", new_stylebox)
+		lvl3_padlock.hide()
 func _on_lvl_1_btn_pressed():
 	GlobalController.lastPlayedLvl = 1
 	SceneSwitcher.switch_scene(lvl1)
