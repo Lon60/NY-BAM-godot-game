627c67870d6857440256ed60f068c22d72603611
Lon60 <gitlon@proton.me>
2024-04-19 08:32:01 +0200

    make geyser better

--- game/items/geyser/geyser.gd
+++ game/items/geyser/geyser.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
-@onready var cpu_particles_2d = $CPUParticles2D
+@onready var fire = $Fire
 
 var on = false
 var count = 1
@@ -9,19 +9,26 @@ var count = 1
 func _on_timer_timeout():
 	if on:
 		if count == 5:
-			cpu_particles_2d.emitting = false
-			cpu_particles_2d.hide()
 			on = false
+			fire.play("end")
 			animated_sprite_2d.play("off")
 			count = 1
 		else:
 			count += 1
 	else:
-		cpu_particles_2d.emitting = true
-		cpu_particles_2d.show()
+		fire.show()
 		on = true
+		fire.play("start")
 		animated_sprite_2d.play("on")
 
 func _on_area_2d_body_entered(body):
 	if on && body && body.is_in_group("player"):
 		body.dead = true
+
+
+
+func _on_fire_animation_finished():
+	if !on:
+		fire.hide()
+	else:
+		fire.play("loop")
